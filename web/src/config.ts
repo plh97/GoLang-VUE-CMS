429b80da54dcf77d76e19e9385d9358a273300bc
plh97 <pengliheng111@gmail.com>
2025-12-19 12:40:29 +0900

    chore: add docs for zh and en

--- web/src/config.ts
+++ web/src/config.ts
@@ -4,14 +4,14 @@ function getBaseURL() {
   const env = process.env.FES_ENV || process.env.NODE_ENV || 'test'
   if (!validEnvs.includes(env)) {
     console.warn(`Invalid environment: ${env}. Falling back to test environment.`)
-    // return 'https://test-manage-api.ohayo.date'
+    return ''
   }
   const envToUrlMap = {
     development: 'http://18.143.194.24:8291',
-    test: 'http://18.143.194.24:8291',
-    prod: 'http://18.143.194.24:8291',
+    // test: '',
+    // prod: '',
   }
-  return envToUrlMap[env as keyof typeof envToUrlMap] ?? 'http://18.143.194.24:8291'
+  return envToUrlMap[env as keyof typeof envToUrlMap] ?? ''
 }
 
 export const baseURL = getBaseURL()
